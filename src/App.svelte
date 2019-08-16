37ce3333efb09d67ea5bca9df5caa1dcf7f74730
shoobah <shoobah@gmail.com>
2019-08-16 03:19:08 +0200

    Icons WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-	export let name;
+  export let name;
+
+  import Icon from "./components/icon.svelte";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
+<Icon type="save" />
