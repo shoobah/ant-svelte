8fea8f0daf963a4221e2d082ff32952ee17e9987
Anders Sjöberg <anders.sjoberg@calabrio.com>
2019-08-20 09:48:43 +0200

    Cleaned up menu

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,26 +51,20 @@
 <nav>
   <ul>
     <li>
-      <a class={segment === undefined ? 'selected' : ''} href=".">home</a>
-    </li>
-    <li>
       <a class={segment === 'icon_doc' ? 'selected' : ''} href="icon_doc">
         Icon
       </a>
     </li>
-    <li>
-      <a class={segment === 'about' ? 'selected' : ''} href="about">about</a>
-    </li>
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li>
+    <!-- <li>
       <a
         rel="prefetch"
         class={segment === 'blog' ? 'selected' : ''}
         href="blog">
         blog
       </a>
-    </li>
+    </li> -->
   </ul>
 </nav>
